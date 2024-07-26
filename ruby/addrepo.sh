65ce310686fe7d2d12675d9fe7786637acd0bf73
Neil Lawrence <lawrennd@gmail.com>
2024-07-26 07:10:29 +0100

    Update with scripts that keep pdfs in main so that gh-pages isn't moving binaries.

--- ruby/addrepo.sh
+++ ruby/addrepo.sh
@@ -1,8 +1,34 @@
 #!/bin/bash
+
+# Check if a volume name is provided
+if [ $# -eq 0 ]; then
+    echo "Please provide a volume name as an argument."
+    exit 1
+fi
+
+volume_name=$1
+
 git init
+
+# Create and switch to gh-pages branch
 git checkout -b gh-pages
-git add .
+
+# Add and commit files
+git add _posts Gemfile *.bib _config.yml README.md index.md
+git commit -m "Add pages for volume $volume_name"
+
+# Create and switch to main branch
+git checkout -b main
+
+# Add and commit pdfs
+git add assets
+git commit -m "Add assets for volume $volume_name"
+
+# Add remote repository 
 git remote add origin git@github.com:mlresearch/$1.git
 git remote -v
-git commit -a -m "Add $1 pages"
-git push --set-upstream origin gh-pages
+
+# Push both branches
+git push -u origin gh-pages
+git push -u origin main
+echo "Repository created and pushed to GitHub."
--- ruby/addrepo_split.sh
+++ ruby/addrepo_split.sh
@@ -1,15 +1,34 @@
 #!/bin/bash
+
+# Check if a volume name is provided
+if [ $# -eq 0 ]; then
+    echo "Please provide a volume name as an argument."
+    exit 1
+fi
+
+volume_name=$1
+
 git init
+
+# Create and switch to gh-pages branch
 git checkout -b gh-pages
-git add README.md _posts index.html *.bib Gemfile _config.yml
+
+# Add and commit files
+git add _posts Gemfile *.bib _config.yml README.md index.md
+git commit -m "Add pages for volume $volume_name"
+
 git remote add origin git@github.com:mlresearch/$1.git
 git remote -v
-git commit -a -m "Add $1 pages"
-git push --set-upstream origin gh-pages
+git push -u origin gh-pages
+
+# Create and switch to main branch
+git checkout -b main
+
+# Add and commit pdfs
 for letter in {a..z}
-do git add $letter*
-   git commit -a -m "Add $1 pages"
-   if git push; then
+do git add assets/$letter*
+   git commit -a -m "Add $volume_name pdfs begining with $letter"
+   if git push -u origin main; then
        continue
    else
        break
